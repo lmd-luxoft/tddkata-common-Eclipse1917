ac977ef6b359f71df37f3dc5777a38ed53707b5b
GaripovIF <Eclipse1917@gmail.com>
2021-04-13 13:32:33 +0300

    добавил реализацию метода sum в Calc

--- src/main/java/tdd/Calc.java
+++ src/main/java/tdd/Calc.java
@@ -3,6 +3,21 @@ package tdd;
 public class Calc {
 
     public int sum(String expression) {
-        throw new UnsupportedOperationException();
+        int result  = -1;
+        if (expression.codePoints().filter(ch -> ch == ',').count() > 1){
+            return result;
+        }
+        try {
+            String[] split = expression.split(",");
+            if (split.length == 1) {
+                result = split[0].trim().equals("") ? 0 : Integer.parseInt(split[0]);
+            }
+            if (split.length == 2) {
+                result = Integer.parseInt(split[0]) + Integer.parseInt(split[1]);
+            }
+        } catch (Exception ignored) {
+            //ignore
+        }
+        return result;
     }
 }
