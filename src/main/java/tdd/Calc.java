71823db9131d078472e6e3fe9d4ef3abf96c9c25
GaripovIF <Eclipse1917@gmail.com>
2021-04-14 10:38:18 +0300

    fix calc

--- src/main/java/tdd/Calc.java
+++ src/main/java/tdd/Calc.java
@@ -5,12 +5,13 @@ public class Calc {
     public int sum(String expression) {
         int result = 0;
         try {
-            long commaCount = expression.codePoints().filter(ch -> ch == ',').count();
-            String[] split = expression.split(",");
+            long commaCount = expression.codePoints().filter(ch -> ch == ',' || ch == '\n').count();
+            String[] split = expression.split("[\n,]+");
+            if (split.length != commaCount + 1) {
+                return -1;
+            }
             if (split.length == 1) {
                 return split[0].trim().equals("") ? result : Integer.parseInt(split[0]);
-            } else if (split.length != commaCount + 1) {
-                return -1;
             }
             for (String s : split) {
                 result += Integer.parseInt(s);
